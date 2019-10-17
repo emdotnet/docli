8e25f7ac81f1ecb411df2d73d7dc0ca41f6adff5
Charles-Henri Decultot <chdecultot@dokos.io>
2019-10-17 09:13:49 +0200

    fix: hard reset branch when switching to avoid merge conflicts

--- bench/app.py
+++ bench/app.py
@@ -348,17 +348,17 @@ def switch_branch(branch, apps=None, bench_path='.', upgrade=False, check_upgrad
 		app_dir = os.path.join(apps_dir, app)
 		if os.path.exists(app_dir):
 			try:
+				exec_cmd("git config --unset-all remote.upstream.fetch", cwd=app_dir)
+				exec_cmd("git config --add remote.upstream.fetch '+refs/heads/*:refs/remotes/upstream/*'", cwd=app_dir)
 				if check_upgrade:
 					version_upgrade = is_version_upgrade(app=app, bench_path=bench_path, branch=branch)
 					if version_upgrade[0] and not upgrade:
 						raise MajorVersionUpgradeException("Switching to {0} will cause upgrade from {1} to {2}. Pass --upgrade to confirm".format(branch, version_upgrade[1], version_upgrade[2]), version_upgrade[1], version_upgrade[2])
 				print("Switching for "+app)
 				unshallow = "--unshallow" if os.path.exists(os.path.join(app_dir, ".git", "shallow")) else ""
-				exec_cmd("git config --unset-all remote.upstream.fetch", cwd=app_dir)
-				exec_cmd("git config --add remote.upstream.fetch '+refs/heads/*:refs/remotes/upstream/*'", cwd=app_dir)
 				exec_cmd("git fetch upstream {unshallow}".format(unshallow=unshallow), cwd=app_dir)
 				exec_cmd("git checkout {branch}".format(branch=branch), cwd=app_dir)
-				exec_cmd("git merge upstream/{branch}".format(branch=branch), cwd=app_dir)
+				exec_cmd("git reset --hard upstream/{branch}".format(branch=branch), cwd=app_dir)
 				switched_apps.append(app)
 			except CommandFailedError:
 				print("Error switching to branch {0} for {1}".format(branch, app))
