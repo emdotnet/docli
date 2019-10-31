0b0e5a9f755c5e5db78614b15973e54a4c33c17f
Charles-Henri Decultot <chdecultot@dokos.io>
2019-10-31 18:57:31 +0100

    feat: Change staging branch

--- bench/commands/utils.py
+++ bench/commands/utils.py
@@ -125,7 +125,7 @@ def backup_all_sites():
 @click.command('release')
 @click.argument('app')
 @click.argument('bump-type', type=click.Choice(['major', 'minor', 'patch', 'stable', 'prerelease']))
-@click.option('--from-branch', default='develop')
+@click.option('--from-branch', default='staging')
 @click.option('--to-branch', default='master')
 @click.option('--remote', default='upstream')
 @click.option('--owner', default='dokos')
