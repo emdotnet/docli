6afe889761a0f1d30eec96a72f2fe1a10978c3ad
Charles-Henri Decultot <chdecultot@dokos.io>
2020-05-14 14:01:44 +0200

    fix: tests + command formatting

--- bench/utils.py
+++ bench/utils.py
@@ -46,6 +46,7 @@ class color:
 	green = '\033[92m'
 	yellow = '\033[93m'
 	red = '\033[91m'
+	silver = '\033[90m'
 
 def is_bench_directory(directory=os.path.curdir):
 	cur_dir = os.path.curdir
@@ -260,26 +261,10 @@ def clone_apps_from(bench_path, clone_from, update_app=True):
 		setup_app(app)
 
 def exec_cmd(cmd, cwd='.'):
-	from .cli import from_command_line
-
-	is_async = False if from_command_line else True
-	if is_async:
-		stderr = stdout = subprocess.PIPE
-	else:
-		stderr = stdout = None
-
-	logger.info(cmd)
-
-	p = subprocess.Popen(cmd, cwd=cwd, shell=True, stdout=stdout, stderr=stderr,
-		universal_newlines=True)
-
-	if is_async:
-		return_code = print_output(p)
-	else:
-		return_code = p.wait()
-
-	if return_code > 0:
-		raise CommandFailedError(cmd)
+	import shlex
+	print("{0}$ {1}{2}".format(color.silver, cmd, color.nc))
+	cmd = shlex.split(cmd)
+	return subprocess.call(cmd, cwd=cwd, universal_newlines=True)
 
 def which(executable, raise_err = False):
 	exec_ = find_executable(executable)
