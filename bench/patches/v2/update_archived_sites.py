5c99c228bb801ff4ff26ecd5b19fdbc438042fcd
Charles-Henri <chdecultot@dokos.io>
2022-02-17 12:01:02 +0100

    fix: PosixPath object has no attribute 'rstrip'

--- bench/patches/v2/update_archived_sites.py
+++ bench/patches/v2/update_archived_sites.py
@@ -7,7 +7,6 @@ patch and try again later.
 Corresponding changes in frappe/frappe via https://github.com/frappe/frappe/pull/15060
 """
 import os
-import shutil
 from pathlib import Path
 
 import click
@@ -35,7 +34,7 @@ def execute(bench_path):
 		os.makedirs(new_directory)
 
 	for archived_site_path in old_directory.glob("*"):
-		shutil.move(archived_site_path, new_directory)
+		archived_site_path.rename(new_directory)
 
 	click.secho(f"Archived sites are now stored under {new_directory}")
 
