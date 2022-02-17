f81cc570949b57bae14b73932acb5c0e29ac77d2
Charles-Henri <chdecultot@dokos.io>
2022-02-17 08:21:09 +0100

    fix: Misc fixes

--- bench/utils.py
+++ bench/utils.py
@@ -1008,6 +1008,8 @@ def generate_command_cache(bench_path='.'):
 		if hasattr(e, "stderr"):
 			print(e.stderr.decode('utf-8'))
 
+	return []
+
 
 def clear_command_cache(bench_path='.'):
 	"""Clears commands cached
