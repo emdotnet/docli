a51c7109ddcd7150f333cc959aaa6a43cb8ce7f9
Charles-Henri <chdecultot@dokos.io>
2022-06-29 07:48:37 +0200

    fix: fixed update_app_states call

--- bench/app.py
+++ bench/app.py
@@ -157,7 +157,15 @@ class AppMeta:
 
 @functools.lru_cache(maxsize=None)
 class App(AppMeta):
-	def __init__(self, name: str, branch: str = None, bench: "Bench" = None, soft_link : bool = False, *args, **kwargs):
+	def __init__(
+		self,
+		name: str,
+		branch: str = None,
+		bench: "Bench" = None,
+		soft_link: bool = False,
+		*args,
+		**kwargs,
+	):
 		self.bench = bench
 		self.soft_link = soft_link
 		self.required_by = None
@@ -256,9 +264,14 @@ class App(AppMeta):
 
 	def update_app_state(self):
 		from bench.bench import Bench
+
 		bench = Bench(self.bench.name)
-		bench.apps.sync(app_dir=self.app_name, app_name=self.name,
-						branch=self.tag, required_list=self.local_resolution)
+		bench.apps.sync(
+			app_dir=self.app_name,
+			app_name=self.name,
+			branch=self.tag,
+			required=self.local_resolution,
+		)
 
 def make_resolution_plan(app: App, bench: "Bench"):
 	"""
