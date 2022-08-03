42e6ad2fe62e8279be5e44d8d9e6477c4099e79e
Charles-Henri <chdecultot@dokos.io>
2022-08-03 14:52:34 +0200

    refactor: Exceptions, variables with pylint

--- bench/utils/__init__.py
+++ bench/utils/__init__.py
@@ -59,9 +59,10 @@ def is_valid_frappe_branch(frappe_path: str, frappe_branch: str):
 	:type frappe_branch: str
 	:raises InvalidRemoteException: branch for this repo doesn't exist
 	"""
-	import git
+	from git.cmd import Git
+	from git.exc import GitCommandError
 
-	g = git.cmd.Git()
+	g = Git()
 
 	if frappe_branch:
 		try:
@@ -70,8 +71,8 @@ def is_valid_frappe_branch(frappe_path: str, frappe_branch: str):
 				raise InvalidRemoteException(
 					f"Invalid branch or tag: {frappe_branch} for the remote {frappe_path}"
 				)
-		except git.exc.GitCommandError:
-			raise InvalidRemoteException(f"Invalid frappe path: {frappe_path}")
+		except GitCommandError as e:
+			raise InvalidRemoteException(f"Invalid frappe path: {frappe_path}") from e
 
 
 def log(message, level=0, no_log=False):
